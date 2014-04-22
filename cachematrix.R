c63bb397083d1280b59c63f9d747d87910dc7164
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    Created the two functions outlined in R Programming Assignment 2 and tested them with a few martices to confirm they are functional. Alos, provided the requested discription of makeCacheMatrix function.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,32 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+          x <<- y
+          m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## This function creates a structure to manage a martix(x) and it's inverse(m). 
+#Allowing the user to set and get the matrix and 'store a precomputed inverse' (cached) using setinverse().
+#This does not compute the inverse, but is reuseable and effecient for recalling it however it is computed 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached inverse")
+    return(m)
+  }
+  mat <- x$get()
+  m <- solve(mat, ...)
+  x$setinverse(m)
+  m
 }
